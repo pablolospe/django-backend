4c0e641b7967328072479be12f424170a0b3bed1
pablolospe <yosoypxl@gmail.com>
2024-05-17 10:38:20 -0300

    un poco de estilos

--- grupo10/web/forms.py
+++ grupo10/web/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 
+# class ClientForm(forms.Form):
+#     name = forms.CharField(label="Nombre", required=True) 
+#     lastname = forms.CharField(label="Apellido", required=True) 
+#     phone = forms.IntegerField(label="Teléfono", required=True)
+#     email = forms.EmailField(label="email", required=True)
+#     address = forms.CharField(label="Dirección", required=True) 
+
+
+
 class ClientForm(forms.Form):
-    name = forms.CharField(label="Nombre", required=True) 
-    lastname = forms.CharField(label="Apellido", required=True) 
-    phone = forms.IntegerField(label="Teléfono", required=True)
-    email = forms.EmailField(label="email", required=True)
-    address = forms.CharField(label="Dirección", required=True) 
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Nombre'}))
+    lastname = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Apellido'}))
+    phone = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'placeholder': 'Teléfono'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Correo electrónico'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Dirección'}))
